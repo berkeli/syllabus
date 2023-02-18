cef0153b3e150493c319a24df129cbe434086c0a
Alasdair Smith <ali@alasdairsmith.co.uk>
2023-02-18 00:07:13 +0000

    Add connecting a DBMS to Render Postgres guide

--- sidebars.js
+++ sidebars.js
@@ -377,6 +377,7 @@ module.exports = {
       "guides/deployment-render/connecting-github",
       "guides/deployment-render/deploying-server",
       "guides/deployment-render/creating-a-postgres-db",
+      "guides/deployment-render/connecting-a-dbms",
     ],
   },
   Accessibility: {
